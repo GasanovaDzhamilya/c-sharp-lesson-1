7b2809bb19cd0cdef7b850d6b712ef7b2064d6e6
Gasanova Dzhamilya <gasanova.dzhami@gmail.com>
2022-12-22 03:06:27 +0300

    lesson one dz 1

---
+++ dz1/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+void isFirstNumberSquareOfSecond(float num1, float num2) 
+{
+    Console.WriteLine("Является ли " + num1 + " квадратом " + num2 + "?");
+    if (num1 == num2 * num2) 
+    {
+        Console.WriteLine("Да, является");
+    } else 
+    {
+        Console.WriteLine("Нет, не является");
+    }
+}
+
+isFirstNumberSquareOfSecond(16, 4);
+isFirstNumberSquareOfSecond(36, 5);
