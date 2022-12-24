695bff26bd57a4b3a740443e8e4a6f266deba20c
Gasanova Dzhamilya <gasanova.dzhami@gmail.com>
2022-12-24 21:00:11 +0300

    lesson one dz5

---
+++ dz5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №5. 
+// Показать последнюю цифру трёхзначного числа.
+
+
+Console.Write("Введите пожалуйста трёхзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a % 10;
+
+if (a>99&&a<1000)
+{
+    // Console.Write("Последня цифра: ");
+    // Console.Write(b);
+    string str = $"Последняя цифра: {b}";
+    Console.Write(str);
+
+}
+else Console.Write("Это не трех значное число");
