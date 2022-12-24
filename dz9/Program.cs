9d81b1f08b696537b61d295280a2d07d5ee1cb8e
Gasanova Dzhamilya <gasanova.dzhami@gmail.com>
2022-12-24 21:21:54 +0300

    lesson one dz9

---
+++ dz9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №9. 
+// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите первое число: ");
+Double FN = Double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+Double SN = Double.Parse(Console.ReadLine()!);
+
+
+if ( FN % SN == 0 ) 
+Console.Write("Первое число кратно второму");
+   
+else 
+Console.WriteLine("Число не кратно");
