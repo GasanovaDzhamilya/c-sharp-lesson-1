e6a9cb05e9eb9a7af139563da4c82ffe4b83ed7f
Gasanova Dzhamilya <gasanova.dzhami@gmail.com>
2022-12-24 21:17:52 +0300

    lesson one dz8

---
+++ dz8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №8.
+// Удалить вторую цифру трёхзначного числа.
+
+//Console.WriteLine("Введите трехзначное число");
+//int a = int.Parse(Console.ReadLine()!);
+//int b = a / 100 * 10 + a % 10;  // 589/100=5, 5*10+50, + 589%10=9, 50+9=59
+//Console.WriteLine(b);
+
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()!);
+int b = a / 100;
+int c = a % 10;
+if (a > 99 && a < 1000) 
+{
+Console.Write(b);
+Console.Write(c);
+}
+else Console.Write("Это не трехзначное число!");
