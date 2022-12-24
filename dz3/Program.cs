a83670dec0d80fdabedd91e7c33b7d1749749854
Gasanova Dzhamilya <gasanova.dzhami@gmail.com>
2022-12-24 20:43:58 +0300

    lesson one dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+
+int number = new Random().Next(10, 99);
+
+Console.WriteLine("Число: " + number);
+
+if (number / 10 > number % 10)
+{
+        Console.WriteLine("Первая цифра больше");
+} else if (number / 10 == number % 10)
+{
+        Console.WriteLine("Цифры одинаковы");
+} else
+{
+        Console.WriteLine("Вторая цифра больше");
+}
